2a8068974aa1a0e256b4a30d3b425b3c1eba085d
edwilau <edwigelauret@gmail.com>
2025-09-24 23:25:01 -0400

    newcicd

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+resource "aws_instance" "server1" {
+    ami = "ami-0360c520857e3138f"
+    instance_type ="t2.micro" 
+    availability_zone = "us-east-1" 
+    tags ={
+        Name= "Dev_server"
+        Environment = Dev
+    } 
+}
